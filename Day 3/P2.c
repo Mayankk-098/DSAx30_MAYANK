d8bea731e50b9934006d902fe13bdae89f7eed77
Mayank Tanwar <160094855+Mayankk-098@users.noreply.github.com>
2025-05-30 23:13:59 +0530

    Added day 3

---
+++ Day 3/P2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+    
+    int k = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+}
+
+int main() {
+    
+    int nums[] = {0, 1, 2, 2, 3, 0, 4, 2};
+    int val = 2;
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    int newLength = removeElement(nums, size, val);
+
+    printf("Output: %d\n", newLength);
+    printf("Array after removal: ");
+    for (int i = 0; i < newLength; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
